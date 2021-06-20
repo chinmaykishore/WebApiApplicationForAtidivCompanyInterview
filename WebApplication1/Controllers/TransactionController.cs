22dbd449d34e7c6fef42205481c93c8f9cfa98c6
chinmaykishore10 <chinmay.kishore@zs.com>
2021-06-20 15:47:50 +0530

    adding createTransaction method

--- WebApplication1/Controllers/TransactionController.cs
+++ WebApplication1/Controllers/TransactionController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -11,35 +13,35 @@ namespace WebApplication1.Controllers
     public class TransactionController : ControllerBase
     {
 
-        IList<Transaction> Transactions = new List<Transaction>()
+        IList<Transaction> transactions = new List<Transaction>()
         {
             new Transaction()
                 {
-                    TransactionId = 1, TransactionDescription = "Bill for hotel", TransactionAmount = 1234, 
-                    TransactionDate = new DateTime(2021, 06, 18), PaymentStatus = "Unbilled" , 
+                    TransactionId = 1, TransactionDescription = "Bill for hotel", TransactionAmount = 1234,
+                    TransactionDate = new DateTime(2021, 06, 18), PaymentStatus = "Unbilled" ,
                     InvoiceGenerationDate = new DateTime(), PaymentDate = new DateTime()
                 },
                 new Transaction()
                 {
-                    TransactionId = 2, TransactionDescription = "Bill for book", TransactionAmount = 435, 
+                    TransactionId = 2, TransactionDescription = "Bill for book", TransactionAmount = 435,
                     TransactionDate = new DateTime(2021, 06, 1), PaymentStatus = "Billed" ,
                     InvoiceGenerationDate = new DateTime(), PaymentDate = new DateTime()
                 },
                 new Transaction()
                 {
-                    TransactionId = 3, TransactionDescription = "bill for chocolate", TransactionAmount = 36, 
+                    TransactionId = 3, TransactionDescription = "bill for chocolate", TransactionAmount = 36,
                     TransactionDate = new DateTime(2021, 06, 5), PaymentStatus = "Paid" ,
                     InvoiceGenerationDate = new DateTime(), PaymentDate = new DateTime()
                 },
                 new Transaction()
                 {
-                    TransactionId = 4, TransactionDescription = "Bill for copy", TransactionAmount = 78, 
+                    TransactionId = 4, TransactionDescription = "Bill for copy", TransactionAmount = 78,
                     TransactionDate = new DateTime(2021, 06, 8), PaymentStatus = "Billed" ,
                     InvoiceGenerationDate = new DateTime(), PaymentDate = new DateTime()
                 },
                 new Transaction()
                 {
-                    TransactionId = 5, TransactionDescription = "Bill for cold drink", TransactionAmount = 89, 
+                    TransactionId = 5, TransactionDescription = "Bill for cold drink", TransactionAmount = 89,
                     TransactionDate = new DateTime(2021, 06, 20), PaymentStatus = "Unbilled" ,
                     InvoiceGenerationDate = new DateTime(), PaymentDate = new DateTime()
                 },
@@ -51,7 +53,7 @@ namespace WebApplication1.Controllers
         {
             _logger = logger;
         }
-
+        /*
         [HttpGet]
         public IList<Transaction> GetAllTransactions()
         {
@@ -70,12 +72,14 @@ namespace WebApplication1.Controllers
             }
             return Transaction;
         }
+        */
 
         [HttpGet]
+        [Route("{id}")]
         public IActionResult GetTransactionDetails1(int id)
         {
             //Return a single Transaction detail  
-            var Transaction = Transactions.FirstOrDefault(e => e.TransactionId == id);
+            var Transaction = transactions.FirstOrDefault(e => e.TransactionId == id);
             if (Transaction == null)
             {
                 return NotFound();
@@ -87,7 +91,31 @@ namespace WebApplication1.Controllers
         public IActionResult GetAllTransactions1()
         {
             //Return list of all Transactions  
-            return Ok(Transactions);
+            return Ok(transactions);
+        }
+
+        [HttpPost]
+        public IActionResult CreateTransaction(NewTransaction newTransaction)
+        //public IActionResult CreateTransaction([FromBody]string newTransactionString)
+        {
+            //var newTransactionDetails = JObject.Parse(newTransaction);
+            //NewTransaction newTransaction = JsonConvert.DeserializeObject<NewTransaction>(newTransactionString);
+            
+            Transaction newTran = new Transaction()
+            {
+                TransactionId = 6,
+                TransactionDescription = newTransaction.TransactionDescription,
+                TransactionAmount = newTransaction.TransactionAmount,
+                TransactionDate = DateTime.Now,
+                PaymentStatus = "Unbilled",
+                InvoiceGenerationDate = new DateTime(),
+                PaymentDate = new DateTime()
+            };
+
+            transactions.Add(newTran);
+
+
+            return Ok();
         }
     }
 }
---
+++ WebApplication1/Models/NewTransaction.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace WebApplication1
+{
+    public class NewTransaction
+    {
+        public double TransactionAmount { get; set; }
+
+        public string TransactionDescription { get; set; }
+
+    }
+}
